0c5db0ad805c1c71b0be771072cde4f80d9a9177
hyerimkim <helim01033@naver.com>
2026-04-08 10:18:14 +0900

    Feat. linkedin 데이터를 이용해서 목데이터의 축약 내용을 AI를 통해 전달받는 코드 작성

--- IceBreaker/ice_breaker.py
+++ IceBreaker/ice_breaker.py
@@ -4,15 +4,10 @@ from dotenv import load_dotenv
 from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 
+from thrid_parties.linkedin import scrape_linkedin_profile
+
 information = """
-미국의 기업인이자 행정가.
-남아공에서 태어나 캐나다를 거쳐 미국에 정착했으며, 현재 미국 텍사스 주 오스틴에 거주하고 있다.
-테슬라, 스페이스X, 스타링크, 뉴럴링크, xAI 홀딩스 등 다수의 첨단 기업을 설립, 보유 및 운영하면서 인류 역사상 최고의 부호로 등극했다. 
-혁신 중심적인 경영과 미래 지향적 비전이 그의 특징으로 평가받고 있다.
-현재 세계 최고의 부자이자 인류 역사상 최고의 부자로, 개인 순자산이 8,000억 달러(한화 약 1,202조원)를 돌파한 인류 역사상 최초의 인물이다. 
-일론 머스크가 설립한 우주 기업 스페이스X의 기업 가치 상승도 재산 증가에 크게 기여했다. 
-포브스는 일론 머스크의 순자산이 최근 기준으로 8,570억 달러, 한화 약 1,288조원에 이른다고 추산했다. 
-테슬라의 경영 미션을 완료할 시 일론 머스크는 인류 역사상 최초로 1조 달러, 한화 약 1,502조원 가량의 수익을 얻게 된다.
+
 """
 
 if __name__ == '__main__':
@@ -27,11 +22,12 @@ if __name__ == '__main__':
 
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    llm = ChatOllama(model="mistral")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    # llm = ChatOllama(model="mistral")
 
     # | 는 앞 결과를 뒤로 넘기는 연산자임
     chain = summary_prompt_template | llm | StrOutputParser()
+    linked_data = scrape_linkedin_profile(linkedin_profile_url="https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json")
 
     res = chain.invoke(input={"information": information})
     print(res)
--- IceBreaker/thrid_parties/linkedin.py
+++ IceBreaker/thrid_parties/linkedin.py
@@ -4,5 +4,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
-    """ """
+def scrape_linkedin_profile(linkedin_profile_url: str):
+    response = requests.get(linkedin_profile_url, timeout=10)
+
+    data = response.json()
+
+    # 불필요한 필드를 제거하는 스니펫
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"): 
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
+
+if __name__ == "__main__":
+    print(scrape_linkedin_profile(linkedin_profile_url="https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/fad4d7a87e3e934ad52ba2a968bad9eb45128665/eden-marco.json"))
