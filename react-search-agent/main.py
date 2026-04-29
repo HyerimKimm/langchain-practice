dbd9140c7d3d4e704c2b2175ac8d924e2156b41f
hyerimkim <helim01033@naver.com>
2026-04-29 23:35:04 +0900

    Feat. Tool 사용해보기

--- react-search-agent/main.py
+++ react-search-agent/main.py
@@ -1,6 +1,31 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+
+@tool
+def search(query: str) -> str: 
+    """
+    인터넷 검색을 하는 툴툴
+    파라미터터: 
+        query: 검색할 질문
+    Returns:
+        검색 결과
+    """
+    print(f"{query}을 검색합니다.")
+    return "서울 날씨는 화창합니다."
+
+llm = ChatOpenAI()
+tools = [search]
+agent = create_agent(model=llm, tools=tools)
+
 def main():
     print("Hello from react-search-agent!")
-
+    result = agent.invoke({"messages": HumanMessage(content="서울 날씨는 어때?")})
+    print(result)
 
 if __name__ == "__main__":
     main()
