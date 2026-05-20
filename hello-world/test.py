0de04cd8eabeb1e518388bc0b7c25319d50b9085
hyerimkim <helim01033@naver.com>
2026-05-20 23:30:47 +0900

    Python 문법 공부용

--- hello-world/test.py
+++ hello-world/test.py
@@ -1,6 +1,13 @@
-class CustopmException(Exception):
-	def __init__(self):
-		super().__init__()
-		print("CustopmException 예외가 발생했습니다.")
-
-raise CustopmException()
+list_a = [1,2,3]
+list_b = [4,5,6]
+print('#리스트')
+print('list_a = ', list_a)
+print("list_a + list_b =", list_a + list_b)
+print("list_a * 3 =", list_a * 3)
+print("len(list_a) = ", len(list_a))
+list_a.append(4)
+print("list_a = ", list_a)
+list_a.insert(0,10)
+print("list_a = ", list_a)
+list_a.extend([7,8,9])
+print("list_a = ", list_a)
