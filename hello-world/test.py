8bdabf8f024662374c6ee4651302021d2c101494
hyerimkim <helim01033@naver.com>
2026-05-21 23:55:23 +0900

    Python 딕셔너리 공부

--- hello-world/test.py
+++ hello-world/test.py
@@ -1,13 +1,6 @@
-list_a = [1,2,3]
-list_b = [4,5,6]
-print('#리스트')
-print('list_a = ', list_a)
-print("list_a + list_b =", list_a + list_b)
-print("list_a * 3 =", list_a * 3)
-print("len(list_a) = ", len(list_a))
-list_a.append(4)
-print("list_a = ", list_a)
-list_a.insert(0,10)
-print("list_a = ", list_a)
-list_a.extend([7,8,9])
-print("list_a = ", list_a)
+dict_a = {
+    "name": "John",
+    "age": 20,
+    "city": "Seoul"
+}
+print(dict_a["name"])
