d7998dbb9f4ed374f026991cc34fd45406abdb86
hyerimkim <helim01033@naver.com>
2026-05-22 19:39:33 +0900

    Feat. in 키워드로 딕셔너리 내부에 키가 있는지 확인하기기

--- hello-world/test.py
+++ hello-world/test.py
@@ -1,6 +1,15 @@
-dict_a = {
-    "name": "John",
-    "age": 20,
-    "city": "Seoul"
+dictionary = {
+    "name": "7D 건조 망고",
+    "type" : "당절임",
+    "ingredient": ["망고", "설탕"],
+    "origin" : "필리핀"
 }
-print(dict_a["name"])
+
+key = input("> 접근하고자 하는 키 : ")
+
+if key in dictionary:
+    print(dictionary[key])
+else:
+    print("존재하지 않는 키입니다.")
+
+
