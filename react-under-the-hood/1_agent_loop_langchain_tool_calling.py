62c1f82206ae8b09cb660984f8f593ee45b1489f
hyerimkim <helim01033@naver.com>
2026-05-22 18:29:44 +0900

    Feat. get_product_price(), apply_discount() 메소드 추가

---
+++ react-under-the-hood/1_agent_loop_langchain_tool_calling.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.chat_models import init_chat_model
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage, SystemMessage, ToolMessage
+
+MAX_ITERATIONS = 10
+MODEL = "qwen3:1.7b"
+
+@tool
+def get_product_price(product: str) -> float:
+    """제품 이름을 입력받아서 제품의 가격을 리턴하는 함수"""
+    prices = { "laptop": 1299.99, "headphones": 149.95, "keyboard": 89.50 }
+    print(f"     >> get_product_price(product={product})를 실행중입니다.")
+    return prices.get(product, 0)
+
+@tool
+def apply_discount(price: float, discount_tier: str) -> float:
+    """
+가격에 할인 등급을 적용하고, 최종 가격을 return한다.
+티어 정보 : bronze, silver, gold.
+	"""
+    print(f"    >> apply_discount(price={price}, discount_tier={discount_tier})를 실행중입니다.")
+    discount_percentages =  {"bronze": 5, "silver": 12, "gold": 23}
+    discount = discount_percentages.get(discount_tier, 0)
+    return round(price * (1 - discount / 100), 2)
