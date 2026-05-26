4058ee07dc73fb90b5be8c1a37fbaa4bea1092e9
hyerimkim <helim01033@naver.com>
2026-05-26 23:32:32 +0900

    Feat. 29 강의 진행중 (4번까지)

--- react-under-the-hood/1_agent_loop_langchain_tool_calling.py
+++ react-under-the-hood/1_agent_loop_langchain_tool_calling.py
@@ -31,7 +31,15 @@ def apply_discount(price: float, discount_tier: str) -> float:
 # --- Agent Loop ---
 @traceable(name="LangChain Agent Loop")
 def run_agent(question: str) -> None:
-    pass
+    tools = [get_product_price, apply_discount]
+    tools_dict = {t.name: t for t in tools}
+
+    llm = init_chat_model(f"ollama: {MODEL}", temperature=0)
+
+    llm_with_tools = llm.bind_tools(tools)
+
+    print(f"Question: {question}")
+    print("-" * 50)
 
 if __name__ == "__main__":
     print("Hello Langchain Agent (.bind_tools)!")
