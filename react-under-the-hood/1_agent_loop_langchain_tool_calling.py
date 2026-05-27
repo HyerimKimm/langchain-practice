44aeb60688295ec1812554b3b8e191109380fa3c
hyerimkim <helim01033@naver.com>
2026-05-27 23:33:38 +0900

    Feat. React agent loop 직접 구현중

--- react-under-the-hood/1_agent_loop_langchain_tool_calling.py
+++ react-under-the-hood/1_agent_loop_langchain_tool_calling.py
@@ -41,6 +41,30 @@ def run_agent(question: str) -> None:
     print(f"Question: {question}")
     print("-" * 50)
 
+    messages = [
+        SystemMessage(content="""
+당신은 쇼핑 도우미입니다. 
+당신은 제품 목록을 확인하는 tool과 할인 적용 tool을 사용할 수 있습니다. 
+[엄격한 규칙] - 당신은 이 규칙을 정확히 따라야만 합니다. 
+1. 제품의 가격을 추측하거나 가정하지 말것
+2. 실제로 가격을 얻으려면 당신은 반드시 get_product_price Tool을 호출해야 합니다. 
+3. 할인 적용된 가격을 계산하기 위해 직접 수학계산 하지 말고, apply_discount Tool을 호출하여 얻어야 합니다.
+4. 사용자의 할인 등급이 없는 경우, 어떤 할인 등급을 적용할 것인지 사용자에게 물어봐야 합니다. 절대 추측하지 마세요.
+"""),
+    HumanMessage(content=question),
+    ]
+
+    for iteration in range(1, MAX_ITERATIONS + 1):
+        print(f"반복문 {iteration} 시작")
+
+        ai_message = llm_with_tools.invoke(messages)
+
+        tool_calls = ai_message.tool_calls
+
+        if not tool_calls: 
+            print(f"최종 답변 : ${ai_message.content}")
+
+
 if __name__ == "__main__":
     print("Hello Langchain Agent (.bind_tools)!")
     print()
